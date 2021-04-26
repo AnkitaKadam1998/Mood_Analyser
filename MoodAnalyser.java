291c9a502e64c7a903d6f4697567ee343be85ba9
komal@komal405.com <komal.jadhav0542@gmail.com>
2021-04-26 23:04:47 +0530

    ADD:Give a message to analyse happy or sad

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,7 +1,23 @@
-class MoodAnalyser
-{
-public static void main(String args[])
-{
-System.out.println("Welcome to Mood Analyser Problem");
-}
+public class MoodAnalyser {
+	
+	String message;
+	public MoodAnalyser() {}
+
+	public MoodAnalyser(String message)
+	{
+		this.message=message;
+	}
+	
+	public String analyseMood()
+	{
+		if(message.contains("Sad"))
+		{
+			return "SAD";
+		}
+		
+		else {
+			return "HAPPY";
+		}
+	}
+	
 }
